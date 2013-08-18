5287703df19c76b7c20ef2c36ce93a8f4e3cb25b
Blapecool <blapecool@gmail.com>
2013-08-18 15:34:39 +0200

    Build once the settings hash and the player list

--- lib/urbanterror/server.rb
+++ lib/urbanterror/server.rb
@@ -3,16 +3,29 @@ require 'pp'
 
 module UrbanTerror
   class Server
+    attr_reader :players, :settings, :last_status
+
     def initialize(server, port=nil, rcon=nil)
       @server = server
       @port = port || 27960
       @rcon = rcon || ''
       @socket = UDPSocket.open
-      @last_status = ''
     end
 
     def update_status
       @last_status = send_command("getstatus")
+
+      # Build the settings hash
+      result = get_status_parts(1).split("\\").reject(&:empty?)
+      @settings = Hash[*result]
+
+      # Build the player list
+      @players = []
+      results = get_status_parts( 2..-1)
+      results.map do |player|
+        player = player.split(" ", 3)
+        @players << { :name => player[2][1..-2], :ping => player[1].to_i, :score => player[0].to_i }
+      end
     end
 
     def get(command)
@@ -21,29 +34,6 @@ module UrbanTerror
 
     def rcon(command)
       send_command("rcon #{@rcon} #{command}")
-    end
-
-    # settings() returns a hash of settings => values.
-    # We /were/ going to accept an optional setting arg, but it would be
-    # doing the same thing and just selecting one from the Hash, so
-    # why not just let the user do server.settings['map'] or whatever.
-    def settings
-      result = get_status_parts(1).split("\\").reject(&:empty?)
-      Hash[*result]
-    end
-    
-    # players() returns a list of hashes. Each hash contains
-    # name, score, ping.
-    def players
-      results = get_status_parts( 2..-1)
-      results.map do |player|
-        player = player.split(" ", 3)
-        {
-          :name => player[2][1..-2],
-          :ping => player[1].to_i,
-          :score => player[0].to_i
-        }
-      end
     end
 
     def get_status_parts(i)
