c89d1cb846bda6babc34ded4b7c070057c3b07bf
Blapecool <Blapecool@gmail.com>
2013-08-19 22:18:34 +0200

    Allow vote calculator

--- lib/urbanterror/constants.rb
+++ lib/urbanterror/constants.rb
@@ -1,5 +1,6 @@
 module UrbanTerror
   MAX_GEAR = 63
+  MAX_ALLOWVOTE = 1073741823
 
   GEAR_TYPES = {
     'knives'   => 0,
@@ -10,6 +11,40 @@ module UrbanTerror
     'autos'    => 16,
     'negev'    => 32
   }
+
+  VOTES_COMMANDS = {
+    'reload'                      => 1,
+    'restart'                     => 2,
+    'map'                         => 4,
+    'nextmap'                     => 8,
+    'kick'                        => 16,
+    'swapteams'                   => 32,
+    'shuffleteams'                => 64,
+    'g_friendlyfire'              => 128,
+    'g_followstrict'              => 256,
+    'g_gametype'                  => 512,
+    'g_waveRespawns'              => 1024,
+    'timelimit'                   => 2048,
+    'fraglimit'                   => 4096,
+    'capturelimit'                => 8192,
+    'g_respawndelay'              => 16384,
+    'redWaveRespawnDelay'         => 32768,
+    'g_blueWaveRespawnDelay'      => 65536,
+    'g_bombExplodeTime'           => 131072,
+    'g_bombDefuseTime'            => 262144,
+    'g_survivorRoundTime'         => 524288,
+    'g_caputureScoreTime'         => 1048576,
+    'g_warmup'                    => 2097152,
+    'g_matchMode'                 => 4194304,
+    'g_timeouts'                  => 8388608,
+    'g_timeoutLength'             => 16777216,
+    'exec'                        => 33554432,
+    'g_swapRoles'                 => 67108864,
+    'g_maxRounds'                 => 134217728,
+    'g_gear'                      => 268435456,
+    'cyclemap'                    => 536870912,
+  }
+
 
   GAME_MODES = {
     0 => ['Free For All',      'FFA'],
--- lib/urbanterror/utilities.rb
+++ lib/urbanterror/utilities.rb
@@ -1,5 +1,4 @@
 module UrbanTerror
-  
   def self.gear_calc(gear_array)
     gear_array.each{ |weapon| raise "No such gear type '#{weapon}'" unless GEAR_TYPES.has_key?(weapon) }
     MAX_GEAR - gear_array.map{|weapon| GEAR_TYPES[weapon] }.reduce(:+)
@@ -10,6 +9,17 @@ module UrbanTerror
     GEAR_TYPES.select{|weapon, gear_val| number & gear_val == 0 }.map(&:first)
   end
   
+  def self.allowvote_calc(votes_array)
+    votes_array.each{ |command| raise "No such voting command '#{command}'" unless VOTES_COMMANDS.has_key?(command) }
+    MAX_ALLOWVOTE - votes_array.map{|command| VOTES_COMMANDS[command] }.reduce(:+)
+  end
+  
+  def self.reverse_allowvote_calc(number)
+    raise "#{number} is outside of the range 0 to #{MAX_ALLOWVOTE}." unless (0..MAX_ALLOWVOTE).include?(number)
+    VOTES_COMMANDS.select{|weapon, gear_val| number & gear_val == 0 }.map(&:first)
+  end
+
+
   def self.gametype_name(number, abbreviate=false)
     raise "#{number} is not a valid gametype." unless GAME_MODES.has_key? number
     GAME_MODES[number][abbreviate ? 1 : 0]
